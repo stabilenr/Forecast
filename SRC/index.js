204153c9c6eaf217f5a5e52895f95547936a8fdf
stabilenr <stabilenr26@gmail.com>
2026-01-03 13:42:02 -0800

    HTML updates made

---
+++ SRC/index.js
@@ -0,0 +1,48 @@
+let apiKey = "109f833e331a8e73of380d4a6cb25ft3";
+
+function searchCity(city) {
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&units=metric&key=${apiKey}`;
+  axios.get(apiURL).then(displayWeather);
+}
+
+function displayWeather(response) {
+  document.querySelector("#current-city").innerHTML = response.data.city;
+  document.querySelector(".current-temperature-value").innerHTML = Math.round(
+    response.data.temperature.current
+  );
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return `${days[day]} ${hours}:${minutes}`;
+}
+
+document.querySelector("#current-date").innerHTML = formatDate(new Date());
